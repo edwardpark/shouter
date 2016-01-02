ed09678e91b5df7c00dc20cf61d5cf41dd435b5d
Eddie Park <eddie.park@id.me>
2016-01-02 16:35:38 -0500

    L1: Add before filter to check if user is logged in

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,18 @@
 class HomesController < ApplicationController
+  before_filter :check_logged_in_user
+
   def show
-  end 
+
+  end
+
+  private
+
+  def check_logged_in_user
+    #this has been added to private because you don't want
+    #this to show up in the logged history of controller actions 
+    if signed_in?
+      redirect_to dashboard_path
+    end
+  end
+
 end
